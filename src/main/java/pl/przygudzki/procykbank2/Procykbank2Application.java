9a0ddaed80db74c27bab77f3a033b149b5ed4cda
kprzygudzki <krzysztofprzygudzki@gmail.com>
2017-06-14 18:49:21 +0200

    Initial commit

---
+++ src/main/java/pl/przygudzki/procykbank2/Procykbank2Application.java
@@ -0,0 +1,12 @@
+package pl.przygudzki.procykbank2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Procykbank2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Procykbank2Application.class, args);
+	}
+}
