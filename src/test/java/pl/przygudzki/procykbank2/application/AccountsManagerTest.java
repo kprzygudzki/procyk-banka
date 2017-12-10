18a27a93ebc66fc0e4e52d1281cede49993aa46d
Krzysiek Przygudzki <kpr@touk.pl>
2017-12-10 13:33:43 +0100

    Refactor AccountController test to AccountManager test

--- src/test/java/pl/przygudzki/procykbank2/application/AccountsManagerTest.java
+++ src/test/java/pl/przygudzki/procykbank2/application/AccountsManagerTest.java
@@ -1,31 +1,34 @@
-package pl.przygudzki.procykbank2.ui;
+package pl.przygudzki.procykbank2.application;
 
 import org.junit.Before;
 import org.junit.Test;
+import pl.przygudzki.procykbank2.application.AccountsManager;
 import pl.przygudzki.procykbank2.application.BalanceDto;
+import pl.przygudzki.procykbank2.infrastructure.Configuration;
 
 import java.math.BigDecimal;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class AccountsControllerTest {
+public class AccountsManagerTest {
 
-	private AccountsController controller;
+	private AccountsManager manager;
 
 	@Before
 	public void setUp() throws Exception {
-		controller = new AccountsController();
+		manager = new Configuration().accountsManager();
 	}
 
 	@Test
 	public void whenQueryingZaarghsBalanceReturnsAnAccountBalanceDto() {
-		BalanceDto balanceDto = controller.queryBalance("zaargh");
+		BalanceDto balanceDto = manager.getBalance("zaargh");
 		assertThat(balanceDto.getBalance()).isEqualTo(BigDecimal.valueOf(-15.0));
 	}
 
 	@Test
 	public void whenQueryingPsotnicksBalanceReturnsAnAccountBalanceDto() {
-		BalanceDto balanceDto = controller.queryBalance("psotnick");
+		BalanceDto balanceDto = manager.getBalance("psotnick");
 		assertThat(balanceDto.getBalance()).isEqualTo(BigDecimal.valueOf(15.0));
 	}
+
 }
